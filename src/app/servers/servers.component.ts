3bf1cabb70cae8e216187a8ab6de81b52e7a0f08
ngeru.sk <ngeru.sk@gmail.com>
2024-01-02 21:49:01 +0300

    debug level

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,10 +7,25 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
   allowNewServer = false;
+  serverCreated = false;
+  serverName: string = '';
+  serverCreationStatus = 'No server was created!';
+  servers = ['Testserver', 'Testserver 2'];
+
 
   constructor(){
     setTimeout(()=>{
       this.allowNewServer = true
     }, 2000);
+  }
+
+  onCreateServer(){
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created with name '+this.serverName;
+    this.servers.push(this.serverName)
+  }
+
+  onNameUpdate(event: any){
+    this.serverName =(<HTMLInputElement>event.target).value;
   }
 }
